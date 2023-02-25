264bf5015abc5643a8cf996b7afdcca5483ad591
denis_khmyrov <denis.e.khmyrov@gmail.com>
2023-02-25 12:53:17 +0300

    fix: fixing errors

--- js/functions.js
+++ js/functions.js
@@ -1,1 +1,29 @@
-git
+function checkStringLength (string, maxLength) {
+  return string.length <= maxLength;
+}
+
+function isPalindrom (string) {
+  const stringWithoutSpaces = string.toLowerCase().replaceAll(' ', '');
+  const reversedString = stringWithoutSpaces.split('')
+    .reverse()
+    .join('');
+  return reversedString === stringWithoutSpaces;
+}
+
+function extractOnlyDigits (string) {
+  let digits = '';
+  for (let i = 0; i < string.length; i++) {
+    if (parseInt(string[i], 10)) {
+      digits += string[i];
+    }
+  }
+  return parseInt(digits, 10);
+}
+
+function myPadStart (string, minLength, pad) {
+  const actualPad = minLength - string.length;
+  if (actualPad <= 0) {
+    return string;
+  }
+  return pad.slice(0, actualPad % pad.length) + pad.repeat(actualPad / pad.length) + string;
+}
