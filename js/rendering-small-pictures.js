acda26de5d3d3ad9e0bd527ebc8daadec83c93a0
denis_khmyrov <denis.e.khmyrov@gmail.com>
2023-03-19 13:27:10 +0300

    feat: rendering comments function added, comments count set up, commentsLoad button appearance set up

--- js/rendering-small-pictures.js
+++ js/rendering-small-pictures.js
@@ -1,9 +1,10 @@
 import { getArrayOfPicturesData } from './data.js';
-import { showBigImage } from './big-picture.js';
+import { showBigImage, renderComments } from './big-picture.js';
 
 const picturesContainer = document.querySelector('.pictures');
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 const picturesData = getArrayOfPicturesData();
+
 
 const picturesFragment = document.createDocumentFragment();
 
