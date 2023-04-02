0766cee86621860a6dc7a159cf3bdf0480d2cbfe
denis_khmyrov <denis.e.khmyrov@gmail.com>
2023-04-02 23:39:12 +0300

    feat: debounce added and code has been refactored

--- js/sort.js
+++ js/sort.js
@@ -2,86 +2,76 @@ import { getData } from './api.js';
 import { getRandomInteger, showAlert, debounce } from './util.js';
 import { renderPictures } from './rendering-small-pictures.js';
 
-const picturesData = getData;
-
 const RANDOM_PICTURES_NUMBER = 10;
 const RERENDER_DELAY = 500;
 
-
-const sortDefaultButton = document.querySelector('#filter-default');
-const sortByRandomButton = document.querySelector('#filter-random');
-const sortByCommentsButton = document.querySelector('#filter-discussed');
+const picturesData = await getData();
 const sortButtons = document.querySelectorAll('.img-filters__button');
+const imageSortButtonsSection = document.querySelector('.img-filters');
 
-async function getRandomPictures () {
+
+function getRandomPictures () {
   const randomPictures = [];
   const randomIndexes = [];
-  const data = await picturesData();
 
   while (randomPictures.length < RANDOM_PICTURES_NUMBER) {
-    const currentIndex = getRandomInteger(0, data.length - 1);
+    const currentIndex = getRandomInteger(0, picturesData.length - 1);
     if (randomIndexes.includes(currentIndex)) {
       continue;
     }
-    randomPictures.push(data[currentIndex]);
+    randomPictures.push(picturesData[currentIndex]);
     randomIndexes.push(currentIndex);
   }
   return randomPictures;
 }
 
-async function renderRandomPictures () {
+const renderDefaultPictures = () => {
   try {
-    const randomPictures = await getRandomPictures();
-    debounce(renderPictures(randomPictures), RERENDER_DELAY);
-    sortByRandomButton.removeEventListener('click', renderRandomPictures);
-  } catch (err) {
-    showAlert(err.message);
-  }
-}
-
-async function renderDefaultPictures () {
-  try {
-    const data = await getData();
-    debounce(renderPictures(data), RERENDER_DELAY);
-    sortByRandomButton.addEventListener('click', renderRandomPictures);
+    renderPictures(picturesData);
     const sortingButtonsSection = document.querySelector('.img-filters');
     sortingButtonsSection.classList.remove('img-filters--inactive');
   } catch (err) {
     showAlert(err.message);
   }
-}
+};
 
-async function getSortedByCommentsPictures () {
-  const data = await getData();
-  const sortedComments = data.slice().sort((a, b) => b.comments.length - a.comments.length);
+function getSortedByCommentsPictures () {
+  const sortedComments = picturesData.slice().sort((a, b) => b.comments.length - a.comments.length);
   return sortedComments;
 }
 
-async function renderDiscussedPictures () {
-  try {
-    const discussedPictures = await getSortedByCommentsPictures();
-    debounce(renderPictures(discussedPictures), RERENDER_DELAY);
-    sortByRandomButton.addEventListener('click', renderRandomPictures);
-  } catch (err) {
-    showAlert(err.message);
-  }
-}
-
-const setActiveClass = (evt) => {
-  sortButtons.forEach((button) =>
-    button.classList.remove('img-filters__button--active')
-  );
-  evt.target.classList.add('img-filters__button--active');
+const activateButton = (button) => {
+  button.classList.add('img-filters__button--active');
 };
 
-const setSortButtonsEventListeners = () => {
-  sortButtons.forEach((button) =>
-    button.addEventListener('click', setActiveClass)
-  );
+const inactivateButtons = () => {
+  sortButtons.forEach((button) => {
+    button.classList.remove('img-filters__button--active');
+  });
+};
 
-  sortByRandomButton.addEventListener('click', renderRandomPictures);
-  sortDefaultButton.addEventListener('click', renderDefaultPictures);
-  sortByCommentsButton.addEventListener('click', renderDiscussedPictures);
+const renderFilteredPictures = () => {
+  const activeSortButtonId = document.querySelector('.img-filters__button--active').id;
+
+  if (activeSortButtonId === 'filter-random') {
+    renderPictures(getRandomPictures());
+  } else if (activeSortButtonId === 'filter-discussed') {
+    renderPictures(getSortedByCommentsPictures());
+  } else {
+    renderPictures(picturesData);
+  }
+};
+
+const debouncePicturesRendering = debounce(renderFilteredPictures, RERENDER_DELAY);
+
+const setSortButtonsEventListeners = () => {
+  imageSortButtonsSection.addEventListener('click', (evt) => {
+    if (evt.target.classList.contains('img-filters__button') && !evt.target.classList.contains('img-filters__button--active')) {
+      inactivateButtons();
+      activateButton(evt.target);
+      debouncePicturesRendering();
+    }
+  });
 };
 
 export { setSortButtonsEventListeners, renderDefaultPictures };
