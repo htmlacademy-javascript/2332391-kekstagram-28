fb3382cae2735a910d2b2d60dd30c029730560bf
Keks <keks@htmlacademy.ru>
2023-04-02 23:42:40 +0300

    :heavy_check_mark: Сборка #11

---
+++ 11/js/main.js
@@ -0,0 +1,6 @@
+import { setFormEventListeners } from './form.js';
+import { setSortButtonsEventListeners, renderDefaultPictures } from './sort.js';
+
+renderDefaultPictures();
+setFormEventListeners();
+setSortButtonsEventListeners();
